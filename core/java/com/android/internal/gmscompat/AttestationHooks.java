LeafOS-Project/android_frameworks_base

gmscompat: Dynamically spoof props for GMS
 * since goolag is on rampage of killing working build FPs

Change-Id: I1caa82d91606353c4f91c9e9a3c7ed653b7ccff5

core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -19,15 +19,33 @@ package com.android.internal.gmscompat;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Field;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.Iterator;
 
 /** @hide */
 public final class AttestationHooks {
+    private static final Executor executor = Executors.newSingleThreadExecutor();
+    private static final Handler mainHandler = new Handler(Looper.getMainLooper());
+
     private static final String TAG = "GmsCompat/Attestation";
+    private static final String API = "https://play.samarv121.dev";
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
     private static final String SAMSUNG = "com.samsung.android.";
@@ -63,6 +81,38 @@ public final class AttestationHooks {
         }
     }
 
+    private static class FetchPropsTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                String responseBody = fetchProps();
+
+                mainHandler.post(() -> spoofBuildGms(responseBody));
+            } catch (Exception e) {
+                Log.e(TAG, "Error making API request", e);
+            }
+        }
+
+        private void spoofBuildGms(String responseBody) {
+            if (responseBody != null) {
+                try {
+                    JSONObject response = new JSONObject(responseBody);
+                    Iterator<String> keys = response.keys();
+
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        String value = response.getString(key);
+                        Log.i(TAG, key + ": " + value);
+
+                        setBuildField(key, value);
+                    }
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error parsing JSON response", e);
+                }
+            }
+        }
+    }
+
     public static void initApplicationBeforeOnCreate(Context context) {
         final String packageName = context.getPackageName();
         final String processName = Application.getProcessName();
@@ -75,18 +125,16 @@ public final class AttestationHooks {
         if (PACKAGE_GMS.equals(packageName)) {
             setBuildField("TIME", String.valueOf(System.currentTimeMillis()));
             if (PROCESS_UNSTABLE.equals(processName)) {
-              sIsGms = true;
-              setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-              setBuildField("PRODUCT", "marlin");
-              setBuildField("DEVICE", "marlin");
-              setBuildField("MODEL", "Pixel XL");
+                sIsGms = true;
+                executor.execute(new FetchPropsTask());
             }
         }
 
-        // Samsung apps like SmartThings, Galaxy Wearable crashes on samsung devices running AOSP
+        // Samsung apps like SmartThings, Galaxy Wearable crashes
+        // on samsung devices running AOSP
         if (packageName.startsWith(SAMSUNG)) {
-          setBuildField("BRAND", "google");
-          setBuildField("MANUFACTURER", "google");
+            setBuildField("BRAND", "google");
+            setBuildField("MANUFACTURER", "google");
         }
     }
 
@@ -99,6 +147,35 @@ public final class AttestationHooks {
         // Check stack for SafetyNet
         if (sIsGms && isCallerSafetyNet()) {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    private static String fetchProps() {
+        try {
+            URL url = new URI(API).toURL();
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            try {
+                urlConnection.setConnectTimeout(10000);
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line).append("\n");
+                }
+
+                reader.close();
+
+                return response.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error making API request", e);
+            return null;
         }
     }
 }

fukiame/exthm_frameworks_base

Change-Id: Ib920ba0b404382344e23b9763251b33ba275634b

core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -1,17 +1,9 @@
 /*
  * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2024 The LeafOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * SPDX-License-Identifier: Apache-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package com.android.internal.gmscompat;
@@ -19,22 +11,35 @@ package com.android.internal.gmscompat;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
+import android.os.Environment;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Iterator;
 
 /** @hide */
 public final class AttestationHooks {
-    private static final String TAG = "GmsCompat/Attestation";
+    private static final String TAG = AttestationHooks.class.getSimpleName();
+    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
     private static final String SAMSUNG = "com.samsung.android.";
+    private static final String DATA_FILE = "gms_certified_props.json";
 
     private static volatile boolean sIsGms = false;
 
-    private AttestationHooks() { }
+    private AttestationHooks() {}
 
     private static void setBuildField(String key, String value) {
         try {
@@ -75,18 +80,42 @@ public final class AttestationHooks {
         if (PACKAGE_GMS.equals(packageName)) {
             setBuildField("TIME", String.valueOf(System.currentTimeMillis()));
             if (PROCESS_UNSTABLE.equals(processName)) {
-              sIsGms = true;
-              setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-              setBuildField("PRODUCT", "marlin");
-              setBuildField("DEVICE", "marlin");
-              setBuildField("MODEL", "Pixel XL");
+                sIsGms = true;
+                setGmsCertifiedProps();
             }
         }
 
-        // Samsung apps like SmartThings, Galaxy Wearable crashes on samsung devices running AOSP
+        // Samsung apps like SmartThings, Galaxy Wearable crashes
+        // on samsung devices running AOSP
         if (packageName.startsWith(SAMSUNG)) {
-          setBuildField("BRAND", "google");
-          setBuildField("MANUFACTURER", "google");
+            setBuildField("BRAND", "google");
+            setBuildField("MANUFACTURER", "google");
+        }
+    }
+
+    private static void setGmsCertifiedProps() {
+        File dataFile = new File(Environment.getDataSystemDirectory(), DATA_FILE);
+        String savedProps = readFromFile(dataFile);
+
+        if (TextUtils.isEmpty(savedProps)) {
+            Log.e(TAG, "No props found to spoof");
+            return;
+        }
+
+        dlog("Found props");
+        try {
+            JSONObject parsedProps = new JSONObject(savedProps);
+            Iterator<String> keys = parsedProps.keys();
+
+            while (keys.hasNext()) {
+                String key = keys.next();
+                String value = parsedProps.getString(key);
+                dlog(key + ": " + value);
+
+                setBuildField(key, value);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing JSON data", e);
         }
     }
 
@@ -100,5 +129,26 @@ public final class AttestationHooks {
         if (sIsGms && isCallerSafetyNet()) {
             throw new UnsupportedOperationException();
         }
+    }
+
+    private static String readFromFile(File file) {
+        StringBuilder content = new StringBuilder();
+
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error reading from file", e);
+            }
+        }
+        return content.toString();
+    }
+
+    private static void dlog(String message) {
+        if (DEBUG) Log.d(TAG, message);
     }
 }